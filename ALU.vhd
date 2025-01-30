ac8c2b319c773782a813dbc19b0597f4a6432e00
nbskvc <nikola.boskovic.2003@gmail.com>
2025-01-30 23:35:36 +0100

    dodate komande: shl, shr, ashr, inc, dec, and, or, not

--- ALU.vhd
+++ ALU.vhd
@@ -1,7 +1,7 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
+
 
 entity ALU is
     Port (
@@ -21,15 +21,31 @@ begin
 		elsif(i_alu_sel = "0001")then
 			o_res <= i_bus;
 		elsif(i_alu_sel = "0010")then
-			o_res <= i_reg_a + i_bus;
+			o_res <= std_logic_vector(unsigned(i_reg_a) + unsigned(i_bus));
 		elsif(i_alu_sel = "0011")then
-			o_res <= i_reg_a - i_bus;
+			o_res <= std_logic_vector(unsigned(i_reg_a) - unsigned(i_bus));
 		elsif(i_alu_sel = "0111")then
 			if(i_reg_a = i_bus)then
 				o_res <= (others => '1');
 			else
 				o_res <= (others => '0');
 			end if;
+		elsif(i_alu_sel = "1000")then--shl
+			o_res <= i_bus(14 downto 0)&'0';
+		elsif(i_alu_sel = "1001")then
+			o_res <= '0'&i_bus(15 downto 1);--shr
+		elsif(i_alu_sel = "1010")then --and
+			o_res <= std_logic_vector(unsigned(i_bus) and unsigned(i_reg_a));
+		elsif(i_alu_sel = "1011")then --or
+			o_res <= std_logic_vector(unsigned(i_bus) or unsigned(i_reg_a));
+		elsif(i_alu_sel = "1100")then --not
+			o_res <= std_logic_vector(not unsigned(i_bus));
+		elsif(i_alu_sel = "1101")then --inc
+			o_res <= std_logic_vector(unsigned(i_bus) + 1);
+		elsif(i_alu_sel = "1110")then --dec
+			o_res <= std_logic_vector(unsigned(i_bus) - 1);
+		elsif(i_alu_sel = "1111")then --ashr
+			o_res <= i_bus(15)&i_bus(15 downto 1);
 		else
 			o_res <= (others => '0');
 		end if;
